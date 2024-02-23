e78aef979ba9fcaacffbfb3344a302b9080b03d3
Ali <jonesal9@oregonstate.edu>
2024-02-22 19:26:12 -0800

    set up service calls to gripper stub script

--- gripper/gripper/suction_gripper.py
+++ gripper/gripper/suction_gripper.py
@@ -3,8 +3,67 @@
 # This node communicates with the arduino mega via Pyserial to control
     # the gripper - vacuum on/off, fingers open/close, etc
 
+# ROS imports
+import rclpy
+from rclpy.node import Node
+from gripper_msgs.srv import GripperVacuum
+from gripper_msgs.srv import GripperFingers
+
+# other imports
+import numpy as np
+
+class SuctionGripper(Node):
+    def __init__(self):
+        # initialize and name the node
+        super().__init__("suction_gripper")
+
+        # set up the services
+        self.vacuum_service = self.create_service(GripperVacuum, 'set_vacuum_status', self.vacuum_service_callback)
+        self.fingers_service = self.create_service(GripperFingers, 'set_fingers_status', self.fingers_service_callback)
+
+        # class variables
+        self.vacuum_on = False
+        self.fingers_engaged = False
+
+    def vacuum_service_callback(self, request, response):
+        """Callback function for the vacuum service. Uses the bool stored in set_vacuum
+            to turn the vacuum on or off
+        """
+        # if the request is True, turn the vacuum on
+        if request.set_vacuum:
+            self.vacuum_on = True
+            self.get_logger().info("Gripper vacuum on")
+        elif not request.set_vacuum:
+            self.vacuum_on = False
+            self.get_logger().info("Gripper vacuum off")
+
+        response.result = True
+        return response
+    
+    def fingers_service_callback(self, request, response):
+        """Callback function for the fingers service. Uses the bool stored in set_fingers
+            to engage or disengage the grippers. This might be better as an action.
+        """
+        # if the request is True, engage the fingers
+        if request.set_fingers:
+            self.fingers_engaged = True
+            self.get_logger().info("Fingers engaged")
+        elif not request.set_fingers:
+            self.fingers_engaged = False
+            self.get_logger().info("Fingers disengaged")
+
+        response.result = True
+        return response
+
 def main(args=None):
-    print("suction_gripper")
+    # initialize
+    rclpy.init(args=args)
+    # instantiate the class
+    my_gripper = SuctionGripper()
+    # hand control over to ROS2
+    rclpy.spin(my_gripper)
+    # shutdown cleanly
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
--- gripper/gripper/user.py
+++ gripper/gripper/user.py
@@ -6,6 +6,8 @@
 # ROS imports
 import rclpy
 from rclpy.node import Node
+from gripper_msgs.srv import GripperVacuum
+from gripper_msgs.srv import GripperFingers
 
 # other imports
 import numpy as np
@@ -17,17 +19,27 @@ class User(Node):
         super().__init__("user")
         self.get_logger().info("Started user node")
 
+        # set up service clients
+        self.vacuum_service_client = self.create_client(GripperVacuum, 'set_vacuum_status')
+        self.get_logger().info("Waiting for gripper vaccuum server")
+        self.vacuum_service_client.wait_for_service()
+
+        self.fingers_service_client = self.create_client(GripperFingers, 'set_fingers_status')
+        self.get_logger().info("Waiting for gripper finger server")
+        self.fingers_service_client.wait_for_service()
+
         # class variables - probably most will be set up as params or loaded from a yaml
         self.roll_values = {"x": [], "y": [], "z": []}
         self.apple_diameter = 80 / 1000 # meters
+        self.actuation_mode = "dual"
 
     def proxy_pick_sequence(self):
 
-        roll_points = 3
+        roll_points = 1
         roll_values = self.generate_roll_values(roll_points)
 
-        yaw_values = [0, 60] # degrees
-        offset_values = [5/1000, 10/1000] # meters
+        yaw_values = [0] # degrees
+        offset_values = [5/1000] # meters
         num_trials = 1
 
         for roll_point in range(roll_points):
@@ -42,35 +54,65 @@ class User(Node):
                         # add noise, if using
                         # apply offset_value - ARM
                         # turn on vacuum - GRIPPER
+                        self.send_vacuum_request(vacuum_status=True)
+                        self.get_logger().info("Gripper vacuum on")
                         # approach apple - ARM
                             # stops early if suction engages - GRIPPER
                         # manually label cup engagement
-                        # close fingers - GRIPPER
+                        # engage fingers - GRIPPER
+                        if self.actuation_mode == "fingers" or self.actuation_mode == "dual":
+                            self.send_fingers_request(fingers_status=True)
+                            self.get_logger().info("Fingers engaged")
                         # move away from apple - ARM
                         # manually label apple pick
-                        # open fingers - GRIPPER
+                        # disengage fingers - GRIPPER
+                        if self.actuation_mode == "fingers" or self.actuation_mode == "dual":
+                            self.send_fingers_request(fingers_status=False)
+                            self.get_logger().info("Fingers disengaged")
                         # turn off vacuum - GRIPPER
+                        self.send_vacuum_request(vacuum_status=False)
+                        self.get_logger().info("Gripper vacuum off")
                         # stop rosbag recording
                         # save metadata
                         pass
+
+    def send_vacuum_request(self, vacuum_status):
+        """Function to call gripper vacuum service.
+            Inputs - vacuum_status (bool): True turns the vacuum on, False turns it off
+        """
+        # build the request
+        request = GripperVacuum.Request()
+        request.set_vacuum = vacuum_status
+        # make the service call (asynchronously)
+        self.vacuum_response = self.vacuum_service_client.call_async(request)
+
+    def send_fingers_request(self, fingers_status):
+        """Function to call gripper fingers service.
+            Inputs - fingers_status (bool): True engages the fingers, False disengages
+        """
+        # build the request
+        request = GripperFingers.Request()
+        request.set_fingers = fingers_status
+        # make the service call (asynchronously)
+        self.fingers_response = self.fingers_service_client.call_async(request)
 
     def generate_roll_values(self, n_values: int, angle_range_deg=135, plot=False):
         """Function that generates points along a 2D arc. Used to calculate the 
             roll points to sample along the apple surface
 
-            Inputs - n_values (int), number of points to generate
+            Inputs - n_values (int): number of points to generate
         """
-
         # set the angular range and step size
         angle_range = np.deg2rad(angle_range_deg)
-        angle_step = angle_range / (n_values-1)
+        angle_step = angle_range / (n_values-1) if n_values > 1 else 1
 
         # set the size of the sampling sphere
         sphere_rad = (self.apple_diameter / 2) * 1.5 # PROBABLY THIS IS A PARAMETER
 
         for i in range(n_values):
             angle = i * angle_step
-            self.get_logger().info(f"Roll point {i} at {angle}deg")
+            angle_deg = np.rad2deg(angle)
+            self.get_logger().info(f"Roll point {i} at {np.round(angle_deg,3)} degrees")
 
             x = 0
             y = -sphere_rad * np.sin(angle)
@@ -88,7 +130,15 @@ class User(Node):
             plt.show()
 
 def main(args=None):
-    print("user")
+    # initialize rclpy
+    rclpy.init(args=args)
+    # instantiate the class
+    my_user = User()
+    my_user.proxy_pick_sequence()
+    # hand control over to ROS2
+    # rclpy.spin(my_user)
+    # shutdown cleanly when finished
+    # rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
